9b220916eec1eb9d3526524e7168bb84268c96ac
wilsonwen77 <125231835+wilsonwen77@users.noreply.github.com>
2025-08-19 17:12:10 +0800

    Update __init__.py

--- src/processors/__init__.py
+++ src/processors/__init__.py
@@ -1,1 +1,10 @@
+"""
+文本處理模組
 
+包含情感分析、關鍵詞提取、文章分類等功能
+"""
+
+from .text_processor import TextProcessor
+from .category_classifier import CategoryClassifier
+
+__all__ = ['TextProcessor', 'CategoryClassifier']
